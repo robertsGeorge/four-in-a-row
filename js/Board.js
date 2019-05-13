d969be512a4b9531ce8518d5279e4822ec4ef808
George Roberts <george.e.roberts@gmail.com>
2019-05-13 11:10:46 +0100

    build createSpaces() method within Board class

--- js/Board.js
+++ js/Board.js
@@ -1,5 +1,30 @@
 class Board {
   constructor() {
-    
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = this.createSpaces();
+  }
+
+  /**
+   * Generates 2D array of spaces.
+   * @param {integer} rows 
+   * @param {integer} cols 
+   * @returns {array} spaces    an array of space objects
+   */
+  createSpaces() {
+    const spaces = [];
+
+    for (let x = 0; x < this.columns; x++) {
+      const column = [];
+
+      for (let y = 0; y < this.rows; y++) {
+        const space = new Space(x, y);     // needs x (1-6) and y (1-7) args
+        column.push(space); 
+      }
+
+      spaces.push(column);
+    }
+
+    return spaces;
   }
 }
