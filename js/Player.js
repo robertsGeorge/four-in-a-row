f3e18e9d3d587c2560d50400ce5e1a86580bea5a
George Roberts <george.e.roberts@gmail.com>
2019-05-13 09:52:05 +0100

    add js files with classes, add constructor methods for Token and Player classes

---
+++ js/Player.js
@@ -0,0 +1,23 @@
+class Player {
+  constructor(name, id, color, active = false) {
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active; // boolean with default value of false
+    this.tokens = this.createTokens(21); 
+  }
+
+  /**
+   * Creates token objects for player
+   * @param     {integer}   num - number of token objects to be created
+   * @returns   {Array}     tokens - an array of new token objects
+   */
+  createTokens(num) {
+    const tokens = [];
+    for (let index = 0; index < num; index++) {
+      const token = new Token(index, this);
+      tokens.push(token);
+    }
+    return tokens;
+  }
+}
